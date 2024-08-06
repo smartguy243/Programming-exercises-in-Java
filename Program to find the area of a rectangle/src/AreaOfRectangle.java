aa6460056cce7066fa98f20c1c3f0ad252662688
SmartGuy <kadiayieraste@mail.ru>
2024-08-06 14:48:44 +0300

    Comment fixed

--- Program to find the area of a rectangle/src/AreaOfRectangle.java
+++ Program to find the area of a rectangle/src/AreaOfRectangle.java
@@ -1,15 +1,16 @@
-/* @author: SmartGuy
-   @description: This program shows multiple way to find the area of a rectangle
-   with length and width
+ /*
+   @author: SmartGuy
+   @description: This program shows multiple ways to find the area of a rectangle
+                 with length and width.
  */
 
 import java.util.Scanner;
 
 
-
-/* 1. Without any interaction with user.
-     @description: Values of length and width are already specify in the program.
-     */
+    /*
+       1. Without any interaction with user.
+       @description: Values of length and width are directly specify in the program.
+    */
 public class AreaOfRectangle {
         public static void main(String[] args) {
             int length = 7;
@@ -21,27 +22,28 @@ public class AreaOfRectangle {
 
 
 
-    /* 2. With user interaction.
-       @description: User are asked to provide the length and width values.
-       and the area is calculated based on the provided values
-     */
+    /*
+       2. With user interaction.
+       @description: User is asked to provide the length and width values
+                     and the area is calculated based on the provided values.
+    */
 class AreaOfRectangle2 {
     public static void main(String[] args) {
         int length;
         int width;
         int area;
 
-        // Getting length and width from the user using Scanner class
+        // Getting length and width from the user using Scanner class.
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the length of the rectangle: ");
         length = scanner.nextInt();
         System.out.println("Enter the width of the rectangle: ");
         width = scanner.nextInt();
 
-        // Calculating the area with provided values
+        // Calculating the area with provided values.
         area = length * width;
 
-        // Displaying results
+        // Displaying results.
         System.out.println("The length of the rectangle is : " +length + "\n"
                          + "The width of the rectangle is : " +width + "\n"
                          + "The area of the rectangle is : " +area  );
--- Program to find the minimum and maximum elements of an array/src/FindMinAndMax.java
+++ Program to find the minimum and maximum elements of an array/src/FindMinAndMax.java
@@ -1,48 +1,52 @@
-/* @author: SmartGuy
-   @description:This program shows multiple way to find maximum and minimum elements
-   of an array
+ /*
+   @author: SmartGuy
+   @description: This program shows multiple ways to find maximum and minimum elements
+                 of an array.
  */
 
-// 1. By using inbuilt method to sort array
+    // 1. Using inbuilt method to sort array
 import java.util.Arrays;
 public class FindMinAndMax {
     public static void main(String[] args) {
         int[] array = {1, 45, 123, 7, 12, 65, 3, 345 , 25, 57};
 
-        // Using inbuilt function to sort array
+        // Invoking the method sort().
         Arrays.sort(array);
 
-        /* After sorting the minimum will be the value at the 0th position
-           and the maximum will be the value at nth position */
+        /*
+           After sorting the minimum will be the value at the 0th position
+           and the maximum will be the value at nth position.
+        */
         System.out.println( "The minimum element : " + array[0] + "\n" +
                 "The maximum element : " + array[array.length-1]);
 
-        /* This is the easiest way to find
-        minimum and maximum elements of and array and this algorithm
-        has a good time complexity [O(n log(n))] and no extra space is used [O(1)]
+        /*
+           This is the easiest way to find minimum and maximum elements of an array.
+           In addition, the following algorithm has a good time complexity [O(n log(n))]
+           and no extra space is used [O(1)].
         */
     }
 }
 
-// 2. Using a for loop and inbuilt methods min() and min() methods
+    // 2. With a for loop using inbuilt methods min() and max().
 
 class FindMinAndMax2 {
 
-    //Method to find the minimum element
+    // Method to find the minimum element.
     static int getMin(int[] arr, int n)
     {
         int result = arr[0];
         for (int i = 1; i < n; i++)
-            result = Math.min(result, arr[i]);
+            result = Math.min(result, arr[i]); // Using min() method.
         return result;
     }
 
-    //Method to find the maximum element
+    // Method to find the maximum element.
     static int getMax(int[] arr, int n)
     {
         int result = arr[0];
         for (int i = 1; i < n; i++)
-            result = Math.max(result, arr[i]); // Using inb
+            result = Math.max(result, arr[i]); // Using max() method.
         return result;
     }
 
@@ -51,12 +55,13 @@ class FindMinAndMax2 {
     {
         int[] array = { 0, 9110, 32, 456, 78, 19 };
         int n = array.length;
-        System.out.println("Minimum element of array: " + getMin(array, n));
-        System.out.println("Maximum element of array: " + getMax(array, n));
+        System.out.println("Minimum element of array: " + getMin(array, n)
+                        + "Maximum element of array: " + getMax(array, n));
 
-     /* This is the best way to find
-        minimum and maximum elements of and array and this algorithm
-        has a good time complexity [O(n)] and no extra space is used [O(1)]
+        /*
+           This is the best way to find minimum and maximum elements of an array
+           because the following algorithm has a better time complexity [O(n)]
+           and no extra space is used [O(1)].
         */
     }
 }
